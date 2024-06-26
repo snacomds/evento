7540d69b50eed849b4bebe007ca999afd1d02b8b
Abdullah Barakat <abdallah771363885@gmail.com>
2024-06-26 20:46:13 +0300

    feat : add services section

--- src/assets/index.js
+++ src/assets/index.js
@@ -2,36 +2,28 @@ import Swiper from 'https://cdn.jsdelivr.net/npm/swiper@11/swiper-bundle.min.mjs
 
 
 const servicesSwiper = new Swiper('.services-swiper', {
-    effect: 'coverflow',
-    grabCursor: true,
-    centeredSlides: true,
-    loop: true,
-    speed: 500,
-    slidesPerView: 'auto',
-    coverflowEffect: {
-      rotate: 0,
-      depth: 500,
-      modifier: 0.5,
-      slideShadows: false,
+  initialSlide:5,
+  slidesPerView: "auto",
+  centeredSlides: true,
+  spaceBetween: 30,
+  
+  slidesPerGroup: 1,
+  grabCursor: true,
+  loop: true,
+  pagination: { 
+    el: ".swiper-pagination",
+    clickable: true,
   },
   autoplay: {
     delay: 3000,
     pauseOnMouseEnter: true,
     disableOnInteraction: 0
 },
-    pagination: {
-      el: '.swiper-pagination',
-      clickable: true,
-    },
-    navigation: {
-      nextEl: '.swiper-button-next',
-      prevEl: '.swiper-button-prev',
-    },
-  });
+});
 
 
-  // setInterval(()=>{
-  //   servicesSwiper.slideNext();
-  // },10000);
+// setInterval(()=>{
+//   servicesSwiper.slideNext();
+// },10000);
 
 
